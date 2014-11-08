007fbf3158ff61af8d0c61b6d2ed7c35c9cfe785
Sebastian Thoss <sebastian.thoss@finanzcheck.de>
2014-11-08 09:52:27 +0100

    Step 2: http://blog.tankist.de/blog/2013/07/17/oauth2-explained-part-2-setting-up-oauth2-with-symfony2-using-fosoauthserverbundle/

---
+++ src/Acme/DemoBundle/Repository/UserRepository.php
@@ -0,0 +1,14 @@
+<?php
+namespace Acme\DemoBundle\Repository;
+
+use Doctrine\ORM\EntityRepository;
+
+/**
+ * Class UserRepository
+ *
+ * @author Sebastian Thoss
+ */
+class UserRepository extends EntityRepository
+{
+
+}
